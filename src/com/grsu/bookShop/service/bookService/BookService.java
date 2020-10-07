e77bff529d24f622d1cb76cbf21fcb04601904fa
ulyanaprovalskaya <ulyana.provalskaya@gmail.com>
2020-10-07 23:31:20 +0300

    .

---
+++ src/com/grsu/bookShop/service/bookService/BookService.java
@@ -0,0 +1,14 @@
+package com.grsu.bookShop.service.bookService;
+
+import com.grsu.bookShop.exception.BooksNotFoundException;
+import com.grsu.bookShop.model.book.Book;
+import com.grsu.bookShop.model.book.BookGenre;
+
+import java.util.List;
+
+public interface BookService {
+
+    List<Book> getBooksByGivenProperty(String author) throws BooksNotFoundException;
+
+    List<Book> getBooksByGivenProperty(BookGenre genre) throws BooksNotFoundException;
+}
